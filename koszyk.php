268f9e0c3708e5b6e2f7d7b6a6926586d97f2ded
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-02-22 12:12:17 +0100

    Wersja aplikacji z https://4programmers.net/Forum/PHP/366779-koszyk_w_sesji

---
+++ koszyk.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title> koszyk </title>
+    <meta http-equiv="Content-Type" content="text/html;
+                charset=UTF-8">
+
+</head>
+<body>
+
+<p><b> zawartość koszyka </b></p>
+<?php
+
+if (isset($_SESSION['koszyk'])) {
+
+    foreach(unserialize($_SESSION['koszyk']) as $produkt) {
+
+        echo "<li>"  .  $produkt . "</li>";
+    }
+
+}
+
+else {
+
+    echo "brak sesji";
+
+
+}
+
+?>
+
+<p><a href="lista.php">przejdz do listy produktów </a></p>
+</body>
+</html>
